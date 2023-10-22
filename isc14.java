6012f4381a5af46f9c6bd7c3bf85927e32148cac
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-10-22 20:14:12 +0700

    Add files via upload

---
+++ isc14.java
@@ -0,0 +1,12 @@
+package isc;
+
+public class isc14 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("perulangan ke-" + i);
+            i++;
+        } while (i <= 10);
+    }
+}
