9cb558b26bda00794a3f99b51bbbf83e2a8c3b01
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-10-25 19:38:09 +0700

    LinkedList

---
+++ isc17.java
@@ -0,0 +1,14 @@
+package isc;
+
+import java.util.LinkedList;
+
+public class isc17 {
+
+    public static void main(String[] args) {
+        LinkedList<String> hewan = new LinkedList<>();
+        hewan.add("Kucing");
+        hewan.add("Singa");
+        hewan.add("Gajah");
+        System.out.println(hewan);
+    }
+}
