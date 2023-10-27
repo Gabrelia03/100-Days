13797dbadef52f5b01e00344d2c53554d57fc2cb
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-10-27 23:19:07 +0700

    Array2D

---
+++ isc19.java
@@ -0,0 +1,14 @@
+package isc;
+
+public class isc19 {
+
+    public static void main(String[] args) {
+        String[][] kontak = {{"Geby", "085298312373"}, {"Biwi", "085456231980"}};
+        for (int i = 0; i < kontak.length; i++) {
+            System.out.println("Nama : " + kontak[i][0]);
+            System.out.println("Kontak : " + kontak[0][1]);
+            System.out.println("--------------------------------");
+        }
+    }
+
+}
