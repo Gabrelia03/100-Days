64af94cfe20f37b72793d772907a854b41946556
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-11-01 20:32:15 +0700

    Konversi string ke integer

---
+++ isc24.java
@@ -0,0 +1,14 @@
+package isc;
+
+public class isc24 {
+
+    public static void main(String[] args) {
+        //Default Variable
+        String air = "100";
+        int api = 100;
+
+        //Konversi Data String ke Integer
+        int padamkan = api - Integer.parseInt(air);
+        System.out.println("Persentase Api: " + padamkan + "%");
+    }
+}
