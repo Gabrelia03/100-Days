4bc282b1aca5146c51ba19c0338d0e7995a6a668
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-11-02 19:12:38 +0700

    Aritmatika

---
+++ isc25.java
@@ -0,0 +1,30 @@
+package isc;
+
+public class isc25 {
+
+    public static void main(String[] args) {
+
+        int a = 25;
+        int b = 5;
+
+        // Operator penjumlahan
+        int penjumlahan = a + b;
+        System.out.println("Hasil penjumlahan: " + penjumlahan);
+
+        // Operator pengurangan
+        int pengurangan = a - b;
+        System.out.println("Hasil pengurangan: " + pengurangan);
+
+        // Operator perkalian
+        int perkalian = a * b;
+        System.out.println("Hasil perkalian: " + perkalian);
+
+        // Operator pembagian
+        int pembagian = a / b;
+        System.out.println("Hasil pembagian: " + pembagian);
+
+        // Operator modulus 
+        int modulus = a % b;
+        System.out.println("Sisa hasil bagi: " + modulus);
+    }
+}
