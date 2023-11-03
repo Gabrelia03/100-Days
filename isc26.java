681430f98d50433f360c8af62435576f326999aa
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-11-03 20:24:38 +0800

    Method Rekursif

---
+++ isc26.java
@@ -0,0 +1,22 @@
+package isc;
+
+public class isc26 {
+
+    public static void main(String[] args) {
+        int n = 8;
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i));
+
+        }
+
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+}
