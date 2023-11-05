2229b6d8d1af57d4acecb61df69b12413948523a
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-11-05 22:05:43 +0800

    Operator Perbandingan

---
+++ isc28.java
@@ -0,0 +1,19 @@
+package isc;
+
+public class isc28 {
+
+    public static void main(String[] args) {
+        int angka1 = 24;
+        int angka2 = 10;
+
+        if (angka1 > angka2) {
+            System.out.println("Angka 1 Lebih Besar Dari Angka 2");
+
+        }else if ( angka1 < angka2 ) {
+            System.out.println("Angka 1 Lebih Kecil Dari Angka 2");
+        }else{
+            System.out.println("Angka 1 Sama Dengan Angka 2");
+        }
+    }
+
+}
