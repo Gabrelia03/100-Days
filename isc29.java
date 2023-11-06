409e6767fe36c1b1ee972c6a488cec672eec74f0
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-11-06 22:11:25 +0800

    Ternary Operator

---
+++ isc29.java
@@ -0,0 +1,14 @@
+package isc;
+
+public class isc29 {
+
+    public static void main(String[] args) {
+        int angka = 7;
+
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+
+        System.out.println("Angka " + angka + " adalah " + hasil);
+
+    }
+
+}
