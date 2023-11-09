edfeed78e00cd5bba9ac6ae19378110d3b333496
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-11-09 21:20:16 +0800

    Perulangan do while

---
+++ isc32.java
@@ -0,0 +1,14 @@
+
+package isc;
+
+
+public class isc32 {
+    public static void main(String[] args) {
+        int i = 10;
+        do {
+            System.out.println("Perulangan ke- "+i);
+            i++;
+        }while (i <= 20);
+    }
+    
+}
