4e8cb8522f1aca62e85c1a3906b75da24462d061
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-11-11 20:45:21 +0700

    Menyimpan data NIM Mahasiswa menggunakan HashSet

--- isc34.java
+++ isc34.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package isc;
 
-/**
- *
- * @author USER
- */
+import java.util.HashSet;
+import java.util.Scanner;
+
+
 public class isc34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<String> nim = new HashSet<>();
+
+        String input;
+        while (!(input = sc.nextLine()).equalsIgnoreCase("Exit")) {
+            if (nim.contains(input)) {
+               System.out.println("NIM " + input + " sudah ada didalam HashSet");
+            } else {
+                nim.add(input);
+            }
+        }
+
+        System.out.println(nim);
+    }
     
 }
