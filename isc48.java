fbe6eb7c167184f164d71c0a32e69cd47d286aa7
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-11-25 19:35:06 +0800

    Mencari angka dalam sebuah array

---
+++ isc48.java
@@ -0,0 +1,46 @@
+package isc;
+
+import java.util.Scanner;
+
+public class isc48 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[50];
+        int arr_count, i, num;
+
+        System.out.print("Input jumlah elemen Array: ");
+        arr_count = sc.nextInt();
+
+        System.out.println("Input " + arr_count + " angka (dipisah dengan enter): ");
+        System.out.println();
+
+        //Simpan setiap angka yang diinput ke dalam array
+        for (i = 0; i < arr_count; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+
+        System.out.println();
+
+        System.out.print("Input angka yang akan dicari: ");
+        num = sc.nextInt();
+
+        //Proses pencarian Array
+        for (i = 0; i < arr_count; i++) {
+            if (arr[i] == num) {
+                System.out.print("Angka ditemukan pada index ke-" + i);
+                break;
+            }
+
+        }
+
+        if (i == arr_count) {
+            System.out.println("Angka tidak ditemukan");
+        }
+
+        System.out.println();
+    }
+
+}
