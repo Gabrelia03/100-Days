12b3ebfe9bf9bf5b1697b77ac17a45c89a32329b
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-11-29 23:08:33 +0800

    Penggunaan break dan continue

---
+++ isc52.java
@@ -0,0 +1,15 @@
+package isc;
+
+public class isc52 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 4) {
+                continue; // Keluar dari loop jika i adalah 3
+            } else if (i == 7) {
+                break;
+            }
+            System.out.println("Nilai i: " + i);
+        }
+    }
+}
