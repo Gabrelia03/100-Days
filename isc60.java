78f4225c9beb3d2e586e224f206b20b4c547cb41
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-12-07 22:38:33 +0800

    Pola persegi panjang bintang

---
+++ isc60.java
@@ -0,0 +1,31 @@
+package isc;
+
+import java.util.Scanner;
+
+public class isc60 {
+
+    public static void main(String[] args) {
+        //Pola persegi panjang bintang
+        Scanner input = new Scanner(System.in);
+
+        int tinggi_persegi,lebar_persegi, i, j;
+
+        System.out.print("Input tinggi persegi: ");
+        tinggi_persegi = input.nextInt();
+
+        System.out.print("Input lebar persegi: ");
+        lebar_persegi = input.nextInt();
+
+        System.out.println();
+
+        for (i = 1; i <= tinggi_persegi; i++) {
+            for (j = 1; j <= lebar_persegi; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
