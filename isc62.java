6beeaff5f0fcd36b791fa9c72b4fe8d7620f60e0
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-12-09 21:43:22 +0800

    Kondisi if-else

---
+++ isc62.java
@@ -0,0 +1,16 @@
+package isc;
+
+public class isc62 {
+
+    public static void main(String[] args) {
+        int nilai1 = 10;
+        int nilai2 = 13;
+
+        if (nilai1 > 5 || nilai2 < 10) {
+            System.out.println("Salah satu kondisi terpenuhi");
+        } else {
+            System.out.println("Kedua kondisi tidak terpenuhi");
+        }
+    }
+
+}
