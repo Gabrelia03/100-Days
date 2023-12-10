c3a4d843688b987ad7d297f8caff7d6724b48d96
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-12-10 20:33:22 +0800

    Mencetak Alfabet

---
+++ isc63.java
@@ -0,0 +1,15 @@
+package isc;
+
+public class isc63 {
+
+    //Mencetak alfabet A-Z
+    public static void main(String[] args) {
+        char alfabet;
+        for (alfabet = 'A'; alfabet <= 'Z'; ++alfabet) {
+            System.out.println(alfabet + " ");
+
+        }
+        System.out.println();
+    }
+
+}
