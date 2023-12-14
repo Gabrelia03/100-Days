3f3f6be53b2baf0ab541a396100b049660d36be5
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-12-14 23:10:53 +0800

    Menampilkan nomor random

---
+++ isc67.java
@@ -0,0 +1,14 @@
+
+package isc;
+
+import java.util.Random;
+
+
+public class isc67 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int nomorAcak = random.nextInt(100);
+        System.out.println("Nomor acak: " + nomorAcak);
+    }
+    
+}
