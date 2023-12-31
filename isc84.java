348b1e859023bbd5a1ac6746f3f66d1f2b8acabe
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-12-31 21:01:17 +0700

    isc84.java

--- isc84.java
+++ isc84.java
@@ -6,24 +6,24 @@ import java.util.Scanner;
 
 public class isc84 {
     public static void main(String[] args) {
-         Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Masukkan kalimat: ");
         String kalimat = input.nextLine();
 
         String[] kata = kalimat.split(" ");
-        int panjangTerpanjang = 0;
+        int panjangKterpanjang = 0;
         String kataTerpanjang = "";
 
         for (String k : kata) {
             int panjangKata = k.length();
-            if (panjangKata > panjangTerpanjang) {
-                panjangTerpanjang = panjangKata;
+            if (panjangKata > panjangKterpanjang) {
+                panjangKterpanjang = panjangKata;
                 kataTerpanjang = k;
             }
         }
 
         System.out.println("Kata terpanjang: " + kataTerpanjang);
-        System.out.println("Panjang kata: " + panjangTerpanjang);
+        System.out.println("Panjang kata: " + panjangKterpanjang);
     }
     
 }
