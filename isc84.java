2579278fecc6e0eed5008c69ec47a04f0ec8cb9c
Gabrelia03 <147223437+Gabrelia03@users.noreply.github.com>
2023-12-31 21:59:24 +0800

    Mencari kata terpanjang dalam sebuah kalimat

---
+++ isc84.java
@@ -0,0 +1,29 @@
+
+package isc;
+
+import java.util.Scanner;
+
+
+public class isc84 {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan kalimat: ");
+        String kalimat = input.nextLine();
+
+        String[] kata = kalimat.split(" ");
+        int panjangTerpanjang = 0;
+        String kataTerpanjang = "";
+
+        for (String k : kata) {
+            int panjangKata = k.length();
+            if (panjangKata > panjangTerpanjang) {
+                panjangTerpanjang = panjangKata;
+                kataTerpanjang = k;
+            }
+        }
+
+        System.out.println("Kata terpanjang: " + kataTerpanjang);
+        System.out.println("Panjang kata: " + panjangTerpanjang);
+    }
+    
+}
